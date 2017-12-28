b372988396b1ad07d2243f7cb35a8655f6481515
Francisco Lopes <franlopt@gmail.com>
2017-12-28 23:16:03 +0000

    Brotli compression
    Takes about an hour to get all results

---
+++ brotli_face_recognition.py
@@ -0,0 +1,58 @@
+import os, brotli
+
+
+def get_references_values():
+    source = os.path.join(os.getcwd(), "orl_faces", "subsets", "reference")
+    ref_dict = {}
+    for dirs in os.listdir(source):
+        for file in os.listdir(os.path.join(source, dirs)):
+            with open(os.path.join(source, dirs, file), 'rb') as original:
+                original_str = original.read()
+                if dirs not in ref_dict.keys():
+                    ref_dict[dirs] = [(file, original_str)]
+                else:
+                    ref_dict[dirs].append((file, original_str))
+    return ref_dict
+
+
+def calculate_best_ncd(ref_dict, test_original_str):
+    test_compressed_str_size = len(brotli.compress(test_original_str))
+    best_ncd = 5
+    best_subject = ""
+    for subject, reference_set in ref_dict.items():
+        ncd = 0
+        for reference in reference_set:
+            ncd += (len(brotli.compress((b''.join([reference[1], test_original_str])))) -
+                    min(test_compressed_str_size, len(brotli.compress((reference[1]))))) / \
+                   (max(test_compressed_str_size, len(brotli.compress((reference[1])))))
+        ncd = ncd / 3.0
+        if best_ncd > ncd:
+            best_ncd = ncd
+            best_subject = subject
+    return best_subject, best_ncd
+
+
+def test_ncd_similarity(reference_values):
+    source = os.path.join(os.getcwd(), "orl_faces", "subsets", "test")
+    test_dict = {}
+    for dirs in os.listdir(source):
+        for file in os.listdir(os.path.join(source, dirs)):
+            with open(os.path.join(source, dirs, file), 'rb') as original:
+                original_str = original.read()
+                subject, best_ncd = calculate_best_ncd(reference_values, original_str)
+                if dirs not in test_dict:
+                    test_dict[dirs] = {file: (subject, best_ncd)}
+                else:
+                    test_dict[dirs][file] = (subject, best_ncd)
+    return test_dict
+
+
+def results():
+    print("\nFace Recognition with Brotli")
+    # dictionary containing the bytes of each file for each subject in the references subset
+    reference_values = get_references_values()
+    return test_ncd_similarity(reference_values)
+
+
+if __name__ == '__main__':
+    results()
--- compressor_results.py
+++ compressor_results.py
@@ -3,12 +3,13 @@ from bzip2_face_recognition import results as bzip2_results
 from zlib_face_recognition import results as zlib_results
 from lzma_face_recognition import results as lzma_results
 from lz4_face_recognition import results as lz4_results
+from brotli_face_recognition import results as brotli_results
 from time import time as tm
 import re
 
 
 def main():
-    compressor_results = [gzip_results, bzip2_results, zlib_results, lzma_results, lz4_results]
+    compressor_results = [gzip_results, bzip2_results, zlib_results, lz4_results, lzma_results, brotli_results]
     for results in compressor_results:
         accuracy = 0
         start = tm()
