2f7173585d560d26e34072192af5f7e3b7dc6dff
Francisco Lopes <franlopt@gmail.com>
2017-12-28 17:06:42 +0000

    prints n' schlit

---
+++ gzip_face_recognition.py
@@ -0,0 +1,67 @@
+import os, gzip, re
+
+
+def get_references_values():
+    source = os.path.join(os.getcwd(), "orl_faces", "subsets", "reference")
+    ref_dict = {}
+    for dirs in os.listdir(source):
+        for file in os.listdir(os.path.join(source, dirs)):
+            f = ("File {}".format(file))
+            with open(os.path.join(source, dirs, file), 'rb') as original:
+                original_str = original.read()
+                if dirs not in ref_dict.keys():
+                    ref_dict[dirs] = [(f, original_str)]
+                else:
+                    ref_dict[dirs].append((f, original_str))
+    return ref_dict
+
+
+def calculate_best_ncd(ref_dict, test_original_str):
+    test_compressed_str_size = len(gzip.compress(test_original_str))
+    best_ncd = 5
+    best_subject = ""
+    for subject, reference_set in ref_dict.items():
+        ncd = 0
+        for reference in reference_set:
+            ncd += (len(gzip.compress((b''.join([reference[1], test_original_str])))) -
+                    min(test_compressed_str_size, len(gzip.compress((reference[1]))))) / \
+                   (max(test_compressed_str_size, len(gzip.compress((reference[1])))))
+        ncd = ncd / 3.0
+        if best_ncd > ncd:
+            best_ncd = ncd
+            best_subject = subject
+    return best_subject, best_ncd
+
+
+def test_ncd_similarity(reference_compressed_values):
+    source = os.path.join(os.getcwd(), "orl_faces", "subsets", "test")
+    test_dict = {}
+    for dirs in os.listdir(source):
+        for file in os.listdir(os.path.join(source, dirs)):
+            with open(os.path.join(source, dirs, file), 'rb') as original:
+                original_str = original.read()
+                subject, best_ncd = calculate_best_ncd(reference_compressed_values, original_str)
+                if dirs not in test_dict:
+                    test_dict[dirs] = {file: (subject, best_ncd)}
+                else:
+                    test_dict[dirs][file] = (subject, best_ncd)
+    return test_dict
+
+
+def main():
+    # dictionary containing the bytes of each file for each subject in the references subset
+    reference_compressed_values = get_references_values()
+    results = test_ncd_similarity(reference_compressed_values)
+    accuracy = 0
+    size = len(results) * 7
+    for subject, test_files in results.items():
+        print("Subject {}".format(re.sub("s", "", subject)))
+        for test_file, result in test_files.items():
+            print("\t{}: Subject {}, NCD: {}".format(test_file, re.sub("s", "", result[0]), result[1]))
+            if subject == result[0]:
+                accuracy += 1
+    print("Positive Results: {}, Accuracy: {}%".format(accuracy, round(accuracy / size * 100, 1)))
+
+
+if __name__ == '__main__':
+    main()
